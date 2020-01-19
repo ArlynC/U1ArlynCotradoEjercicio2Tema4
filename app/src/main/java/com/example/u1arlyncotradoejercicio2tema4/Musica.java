05af0a17a201ead5df478a0d20b2626a78cdccc8
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-18 23:07:17 -0500

    añadiendo musica

--- app/src/main/java/com/example/u1arlyncotradoejercicio2tema4/Musica.java
+++ app/src/main/java/com/example/u1arlyncotradoejercicio2tema4/Musica.java
@@ -3,16 +3,20 @@ package com.example.u1arlyncotradoejercicio2tema4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class Musica extends AppCompatActivity {
 
+    MediaPlayer reproductor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musica);
+        reproductor = MediaPlayer.create(this, R.raw.hozier_work);
+        reproductor.start();
     }
 
     public void desinfectar(View view) {
