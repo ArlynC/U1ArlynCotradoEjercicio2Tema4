8f856702b92bb36006d81a0b8341b3644fbfd1d6
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-18 23:01:56 -0500

    actividad desinfectar

--- app/src/main/java/com/example/u1arlyncotradoejercicio2tema4/Musica.java
+++ app/src/main/java/com/example/u1arlyncotradoejercicio2tema4/Musica.java
@@ -2,7 +2,10 @@ package com.example.u1arlyncotradoejercicio2tema4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Musica extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Musica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musica);
+    }
+
+    public void desinfectar(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.avast.android.mobilesecurity")));
     }
 }
